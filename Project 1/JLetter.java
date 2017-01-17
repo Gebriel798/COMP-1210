169350b026134f3fffc7425fc7f79f203817cfc1
Grant Haislip <granthaislip@gmail.com>
2017-01-16 22:07:53 -0600

    Add files via upload

---
+++ Project 1/JLetter.java
@@ -0,0 +1,28 @@
+/**
+* This program prints the letter J composed of the word "JAVA".
+* 1210 Project 1.
+* 
+* @author Grant Haislip - Lab 7
+* @version 8/25/2016
+*/
+public class JLetter
+{
+   /**
+   * Prints the letter J composed of the word "JAVA".
+   * 
+   * @param args Command line arguments (not used).
+   */
+   public static void main(String[] args)
+   {
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("J     JAVA");
+      System.out.println("JA    JAVA");
+      System.out.println(" JAVAJAVA");
+      System.out.println("  JAVAJA");
+   }
+}
---
+++ Project 1/MyLifeGoals.java
@@ -0,0 +1,36 @@
+/**
+* This program prints my name, and my short-term, mid-term, and long-term goals.
+* 1210 Project 1.
+*
+* @author Grant Haislip - Lab 7
+* @version 8/25/2016
+*/
+public class MyLifeGoals
+{
+   /** 
+   * Prints my name, and my short-term, mid-term, and long-term goals.
+   * 
+   * @param args Command line arguments (not used).
+   */
+   public static void main(String[] args)
+   {
+      System.out.println("Grant Haislip");
+      System.out.println();
+      System.out.println("My short-term life goal is "
+         + "to get sufficient grades and knowledge in my college "
+         + "classes so that I can become a "
+         + "proper engineer in my future profession."
+         + " Having learning experiences like interships and CO-OPs"
+         + " are somethings that I want to persue as well");
+      System.out.println("My mid-term life goal is "
+         + "to get a good job right after college "
+         + "that is enjoyable and can sustain my expenses. "
+         + "Also, I want to be able to live close to my family"
+         + ", so I want my job to locate me in or near Alabama");
+      System.out.println("My long-term life goal is to live a happy"
+         + " life and impact others in a postive way. Whether this be caring"
+         + " for the ones I love or just trying to be a good"
+         + " person everyday, how I view a reflection of my life"
+         + " will depend on how much I help others over time");
+   }
+}
